919fa366da12e1aa9eadd5748882dd8e95327680
FUEGO62 <jesseolaisrael@gmail.com>
2024-08-22 10:30:01 +0100

    Add password.py file

---
+++ classtask/password.py
@@ -0,0 +1,17 @@
+'''
+prompt the user for their password
+store it
+count the number of characters
+declare the symbol
+multiply the symbol by the count
+display result
+
+'''
+
+password=input("Enter your password: ")
+
+number= len(password)
+
+figure = '*'
+
+print(figure*number)
