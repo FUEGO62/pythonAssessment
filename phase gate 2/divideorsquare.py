4f58e4f72003bd9a64b6593675bb6b71a6f9232a
FUEGO62 <jesseolaisrael@gmail.com>
2024-10-04 13:28:15 +0100

    add file

---
+++ phase gate 2/divideorsquare.py
@@ -0,0 +1,9 @@
+def divideorsquare(number):
+
+	if type(number) == type("jesse"): raise TypeError	
+
+	if number< 0: raise ValueError	
+
+	elif  number > 0 and number%5 == 0: return number**0.5
+
+	else:  return number%5
---
+++ phase gate 2/divideorsquare_test.py
@@ -0,0 +1,33 @@
+import unittest
+import divideorsquare
+
+class divideorsquare_test(unittest.TestCase):
+
+	def test_that_divideorsquare_function_exists(self):
+
+		self.assertEqual(divideorsquare.divideorsquare(11),1)
+
+
+	def test_that_divideorsquare_function_returns_correct_value(self):
+
+		self.assertEqual(divideorsquare.divideorsquare(11),1)
+
+	
+	
+	def test_that_divideorsquare_function_works_with_floating_point_values(self):
+
+		self.assertEqual(divideorsquare.divideorsquare(4.2),(4.2%5))
+	
+	def test_that_divideorsquare_function_Raises_error_with_String(self):
+
+		self.assertRaises(TypeError,divideorsquare.divideorsquare("semicolon"))
+		
+
+	
+	def test_that_divideorsquare_function_Raises_error_with_Negatives(self):
+
+		self.assertRaises(ValueError,divideorsquare.divideorsquare(-2))
+
+
+
+	
